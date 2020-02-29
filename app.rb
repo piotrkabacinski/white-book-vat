97fc662da33be92cad6afbfdde2e35877f280972
Piotr Kabaciński <piotrkabacinski@users.noreply.github.com>
2020-02-29 12:19:39 +0100

    Create google API key and Gemfile

--- app.rb
+++ app.rb
@@ -1,1 +1,10 @@
-puts "Hello, White Book!"
+require 'json'
+require 'net/http'
+
+# Bundler.require sets up the load paths and automatically requires every dependency,
+# saving you from having to manually require each one.
+require "bundler"
+Bundler.setup(:default)
+
+FACT = JSON.parse Net::HTTP.get('cat-fact.herokuapp.com', '/facts/5c72d9b651021f001415f00c')
+puts FACT['text']
